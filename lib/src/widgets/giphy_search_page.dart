edee633b18be8e0575b4e5b14c234fe5707b13f2
Guy <ghaisanlatheef98@gmail.com>
2019-09-24 09:06:57 +0500

    second commit revised

--- lib/src/widgets/giphy_search_page.dart
+++ lib/src/widgets/giphy_search_page.dart
@@ -7,6 +7,7 @@ import 'package:flutter/material.dart';
 class GiphySearchPage extends StatefulWidget {
   final Widget title;
   const GiphySearchPage({this.title});
+
   @override
   _GiphySearchPageState createState() => _GiphySearchPageState();
 }
